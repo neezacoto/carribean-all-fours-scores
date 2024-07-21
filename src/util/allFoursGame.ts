0c55c3b98a54d252925f3f2d838b7f2d9a515d41
Christian Rudder <74576449+neezacoto@users.noreply.github.com>
2024-07-20 22:11:51 -0400

    data entry

--- src/util/allFoursGame.ts
+++ src/util/allFoursGame.ts
@@ -306,7 +306,7 @@ export class Tournament {
         "1:37pm"
     );
     tournament.addGameToRound(
-        1,
+        2,
         divisions.STRIKERS,
         17,
         6,
@@ -318,14 +318,179 @@ export class Tournament {
     );
     tournament.addGameToRound(
         2,
-        divisions.STRIKERS,
+        divisions.WHY_WORRY,
         17,
         4,
-        divisions.AIR_FORCE_ONE,
+        divisions.UNITY,
+        13,
+        2,
+        "no entry",
+        "5:50pm"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.RAIDERS,
+        17,
+        3,
+        divisions.UNTOUCHABLES,
+        15,
+        8,
+        "3:00pm",
+        "5:50pm"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.GAME_OF_THRONES,
+        17,
+        3,
+        divisions.UNTOUCHABLES,
         9,
         1,
+        "3:05pm",
+        "5:44pm"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.NEW_MILLENNIUM,
+        17,
+        3,
+        divisions.MONTREAL,
+        8,
+        0,
+        "2:45pm",
+        "5:20pm"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.SOCOSA,
+        17,
+        3,
+        divisions.REBELS,
+        6,
+        1,
         "3:00pm",
-        "5:58pm"
+        "5:21pm"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.GAMBLERS,
+        17,
+        7,
+        divisions.CARIB,
+        15,
+        8,
+        "no entry",
+        "6:00pm"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.ORIGINAL_EAGLES,
+        17,
+        7,
+        divisions.BLACK_EAGLES,
+        13,
+        0,
+        "no entry",
+        "no entry"
+    );
+    tournament.addGameToRound(
+        2,
+        divisions.GUYANA,
+        17,
+        7,
+        divisions.NEW_JERSEY_UNITED,
+        13,
+        0,
+        "no entry",
+        "no entry"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.NEW_JERSEY_UNITED,
+        17,
+        8,
+        divisions.STRIKERS,
+        12,
+        5,
+        "8:15pm",
+        "9:29pm"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.NEW_MILLENNIUM,
+        17,
+        5,
+        divisions.UNITY,
+        14,
+        8,
+        "no entry",
+        "9:26pm"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.CARIB,
+        17,
+        7,
+        divisions.RAIDERS,
+        13,
+        4,
+        "no entry",
+        "no entry"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.GUYANA,
+        17,
+        3,
+        divisions.AIR_FORCE_ONE,
+        12,
+        7,
+        "no entry",
+        "no entry"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.WHY_WORRY,
+        17,
+        2,
+        divisions.MONTREAL,
+        6,
+        2,
+        "no entry",
+        "9:08pm"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.UNTOUCHABLES,
+        17,
+        1,
+        divisions.JUST_FOR_YOU,
+        12,
+        7,
+        "no entry",
+        "9:05pm"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.SOCOSA,
+        17,
+        3,
+        divisions.ORIGINAL_EAGLES,
+        7,
+        1,
+        "no entry",
+        "no entry"
+    );
+    tournament.addGameToRound(
+        3,
+        divisions.BLACK_EAGLES,
+        17,
+        3,
+        divisions.REBELS,
+        6,
+        1,
+        "no entry",
+        "8:29pm"
     );
 
     return tournament;
